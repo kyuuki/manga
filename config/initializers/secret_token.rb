ce77353da3f2bd23f5275001614cb1bf4f027d7d
kyuuki <kyuuki@sakura.sakura.ne.jp>
2013-02-11 11:46:20 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Manga::Application.config.secret_token = 'f7733aa4d2fdb66cba2a80d856d2f7f24161adb72eb86a49cddd1edd5b747c29ee7bd6c2b572e78a1d3d050e50a4fef2c0e54046cbc0680705aa39b810eaeb30'
