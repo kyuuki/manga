7c106402cfa76cbc2b218d6b57ae3b9140de3d03
kyuuki <kyuuki@sakura.sakura.ne.jp>
2013-05-24 13:10:32 +0900

    Modify a error on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Manga::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application_iphone.css application_iphone.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
